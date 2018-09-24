0ca2d9f2a2c773e2ee8d9de08f1ad5a632788856
Kyle McCracken <kmccracken@olin.edu>
2018-09-23 20:36:17 -0400

    getting pandas working and making code file

--- TurtleCode.py
+++ TurtleCode.py
@@ -1,22 +1,6 @@
 from modsim import *
 from pandas import *
 
-x_array = linspace(1,10,10)
-y_array = linspace(1,10,10)
-
-data = TimeSeries()
-
-for t in linspace(1,10,10):
-    data[t] = t
-
-plot(data)
-savefig('figs/testplot.pdf')
-
-
-
-from modsim import *
-from pandas import *
-
 testDataframe = pandas.read_csv('data/testData.csv',index_col=0)
 print(testDataframe)
 testDataframe.columns = ['data']
