cd93f1ca77b9337d78aedd8a01133ef045a5fc8e
Kyle McCracken <kmccracken@olin.edu>
2018-09-30 13:50:18 -0400

    finishing code and notebook

--- TurtleCode_NewData.py
+++ TurtleCode_NewData.py
@@ -1,20 +1,13 @@
 from modsim import *
 from pandas import *
 
-system = System(nestR = .002, rr1 = .01, rr2 = .59, rr3 = .28, rr4 = .16, rr5 = .047, rr6 = .015, startYear = 1983, endYear = 2020, startPop = 23)
-state = State(population = 2000, r1T = TimeSeries(), r2T = TimeSeries(), r3T = TimeSeries(), r4T = TimeSeries(), r5T = TimeSeries(), r6T = TimeSeries(), newN = TimeSeries())
+systemTest = System(nestR = .002, rr1 = .01, rr2 = .59, rr3 = .28, rr4 = .16, rr5 = .047, rr6 = .015, startYear = 1984, endYear = 2020, startPop = 200)
+stateTest = State(population = 2000, r1T = TimeSeries(), r2T = TimeSeries(), r3T = TimeSeries(), r4T = TimeSeries(), r5T = TimeSeries(), r6T = TimeSeries(), newN = TimeSeries())
 
-turtleDataframe = pandas.read_csv('data/Garner_LeatherbackRemigrationIntervalsData_StCroix.csv',index_col = 0, header = 0)
-nestingTurtleData = turtleDataframe['Turtles']
-ri1 = turtleDataframe['RI1']
-ri2 = turtleDataframe['RI2']
-ri3 = turtleDataframe['RI3']
-ri4 = turtleDataframe['RI4']
-ri5 = turtleDataframe['RI5']
-ri6 = turtleDataframe['RI6up']
-new = turtleDataframe['Unknown']
+turtleDataframe2 = pandas.read_csv('data/NOAA_LeatherbackNestsData.csv',index_col = 0, header = 0)
+nestsData = turtleDataframe2['Nests']
 
-def stepmk1(system,state,t,fiveYearPop, sixAndBefore, sabAge):
+def stepmk1(system,state,t,fiveYearPop, sixAndBefore, sabAge, r1T, r2T, r3T, r4T, r5T, r6T):
     if t < int(system.startYear) + 2:  #for the first two years, we want a solid base of new nesters
         newNesters = system.startPop
     else:
@@ -23,20 +16,20 @@ def stepmk1(system,state,t,fiveYearPop, sixAndBefore, sabAge):
 
     R1 = fiveYearPop[1] * system.rr1    #fiveYearPop = the population from the previous 5 years
                                         #i.e. fiveYearPop[1] is the population 1 year ago
-    state.r1T[t] = R1                         #fills the system's Timeseries for return numbers every x years for each year
+    r1T[t] = R1                         #fills the system's Timeseries for return numbers every x years for each year
     R2 = fiveYearPop[2] * system.rr2
-    state.r2T[t] = R2
+    r2T[t] = R2
     R3 = fiveYearPop[3] * system.rr3
-    state.r3T[t] = R3
+    r3T[t] = R3
     R4 = fiveYearPop[4] * system.rr4
-    state.r4T[t] = R4
+    r4T[t] = R4
     R5 = fiveYearPop[5] * system.rr5
-    state.r5T[t] = R5
+    r5T[t] = R5
     if t > int(system.startYear) + 5:       #once we have 6 years of data we can include the tuned rate from 6 years ago and before
         R6up = (sixAndBefore / sabAge) * system.rr6     #sixAndBefore/sabAge is the average population from the beginning of the model
     else:
         R6up = 0
-    state.r6T[t] = R6up
+    r6T[t] = R6up
 
     nesters = newNesters + R1 + R2 + R3 + R4 + R5 + R6up       #sums all the nesters added this year
     return nesters      #returns the number of nesters this year
@@ -67,7 +60,7 @@ def runSimulation(system,state,step):
                 fiveYearPop[p] = results[t-p]   #Fills fiveYearPop in reverse so that the first instance is 1 year ago, etc.
 
         #Runs step functions, calculating nesting population based on previous year populations
-        results[t] = step(system,state,t, fiveYearPop, sixAndBefore, sabAge)
+        results[t] = step(system,state,t, fiveYearPop, sixAndBefore, sabAge, state.r1T, state.r2T, state.r3T, state.r4T, state.r5T, state.r6T)
     return results
 
 def plotResults(turtleData, timeseries, title, colorin):    #Plotting function
@@ -80,17 +73,5 @@ def plotResults(turtleData, timeseries, title, colorin):    #Plotting function
 
 plt.figure(11) #plots new figure with id 11
 
-plotResults(nestingTurtleData, runSimulation(system,state,stepmk1), 'Model Mk1','purple')   #Plots nesting population data against simulated nesting population
-savefig('figs/Returnyearcomparisons/nestingTurtleData-ModelMk1-.png')
-
-for num in range(1,7): #Plots six timesSeries, each of which has the nesting population for a give return interval, against the data for each return interval
-    plotData = 'ri' + str(num)
-    stateData = 'state.r' + str(num) + 'T'
-    plotTitle = 'r' + str(num) + 'graph'
-    plt.figure(num)
-    plotResults(eval(plotData),eval(stateData),plotTitle,'blue')
-    savefig('figs/Returnyearcomparisons/returnRateTurtleData-ModelMk1-' + str(num) + '.png')
-
-plt.figure(10)
-plotResults(new,state.newN,'New Nesters','blue')    #Plots the new nesters every year against the data
-savefig('figs/Returnyearcomparisons/newNestersTurtleData-ModelMk1-10.png')
+plotResults(nestsData, runSimulation(systemTest,stateTest,stepmk1), 'Model Mk1','purple')   #Plots nesting population data against simulated nesting population
+savefig('figs/NewData/nestsData-ModelMk1-.png')
